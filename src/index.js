e06da9f88cd0bbabd8559145bf1686287f54708b
ludovic <wulfy72@yahoo.fr>
2018-06-21 09:36:21 +0200

    new display proposal to view score

--- src/index.js
+++ src/index.js
@@ -30,6 +30,216 @@ const cronJobTime = process.env.CRON_TIME || "*/15 * * * * *";
 let matches = {};
 let live = false;
 
+const getNow = () => {
+  return IS_DEV ? moment("2018-06-18T14:00") : moment();
+};
+
+const sendMessageQueue = new Queue(
+  ({ match, event, msg, attachments = [] }, done) => {
+    const homeTeam = match.getHomeTeam();
+    const awayTeam = match.getAwayTeam();
+
+    let text = ``;
+
+    if (event) {
+      const homeScore = get(event, "HomeGoals", 0);
+      const awayScore = get(event, "AwayGoals", 0);
+
+      text += ` ${homeTeam.getName(true)} *${homeScore}-${awayScore}* ${awayTeam.getName(true,true)} `;
+    }
+    
+    text += `\n${msg}`;
+
+    slackhook.send({
+      text,
+      attachments
+    });
+
+    done();
+  },
+  { afterProcessDelay: 1000 }
+);
+
+const handleMatchStartEvent = (match, event) => {
+  console.log("New event: matchStart");
+
+  sendMessageQueue.push({ match, event, msg: ":zap: *C'est parti !*" });
+};
+
+const handleMatchEndEvent = (match, event) => {
+  console.log("New event: matchEnd");
+
+  sendMessageQueue.push({
+    match,
+    event,
+    msg: `:stopwatch: *Fin du match* (${event.MatchMinute})`
+  });
+};
+
+const handleFirstPeriodEndEvent = (match, event) => {
+  console.log("New event: firstPeriodEnd");
+
+  sendMessageQueue.push({
+    match,
+    event,
+    msg: `:toilet: *Fin de la première période* (${event.MatchMinute})`
+  });
+};
+
+const handleSecondPeriodStartEvent = (match, event) => {
+  console.log("New event: secondPeriodStart");
+
+  sendMessageQueue.push({ match, event, msg: ":runner: *C'est reparti !*" });
+};
+
+const handleCardEvent = (match, event, team, type) => {
+  console.log("New event: card");
+
+  const playerName = team.getPlayerName(event.IdPlayer);
+
+  let msg = "";
+
+  switch (type) {
+    // case "yellow":
+    //   msg += ":large_orange_diamond: *Carton jaune*";
+    //   break;
+    case "red":
+      msg += ":red_circle: *Carton rouge*";
+      break;
+    case "yellow+yellow":
+      msg +=
+        ":large_orange_diamond::large_orange_diamond: *Carton rouge* (deux jaunes)";
+      break;
+    default:
+      return;
+  }
+
+  msg += ` pour ${playerName} ${team.getFlag()} (${event.MatchMinute})`;
+
+  sendMessageQueue.push({
+    match,
+    event,
+    msg
+  });
+};
+
+const handleOwnGoalEvent = (match, event, team) => {
+  const oppTeam = match.getOppositeTeam(team);
+
+  const msg = `:soccer: *Goooooal! pour ${oppTeam.getNameWithDeterminer(
+    null,
+    true
+  )}* (${event.MatchMinute})`;
+
+  const attachments = [
+    {
+      text: `${team.getPlayerName(
+        event.IdPlayer,
+        true
+      )} marque contre son camp :face_palm:`,
+      color: "danger",
+      actions: [
+        {
+          type: "button",
+          text: ":tv: Accéder au live",
+          url: "http://neosportek.blogspot.com/p/world-cup.html"
+        }
+      ]
+    }
+  ];
+
+  sendMessageQueue.push({ match, event, msg, attachments });
+};
+
+const handleGoalEvent = (match, event, team, type) => {
+  console.log("New event: goal");
+
+  if (type === "own") {
+    handleOwnGoalEvent(match, event, team);
+    return;
+  }
+
+  const playerName = team.getPlayerName(event.IdPlayer);
+
+  const msg = `:soccer: *Goooooal! pour ${team.getNameWithDeterminer(
+    null,
+    true
+  )}* (${event.MatchMinute})`;
+
+  let attachments = [];
+
+  switch (type) {
+    case "freekick":
+      attachments.push({
+        text: `But de ${playerName} sur coup-franc`
+      });
+      break;
+    case "penalty":
+      attachments.push({
+        text: `But de ${playerName} sur penalty`
+      });
+      break;
+    default:
+      attachments.push({
+        text: `But de ${playerName}`
+      });
+  }
+
+  attachments[0].color = "good";
+  attachments[0].actions = [
+    {
+      type: "button",
+      text: ":tv: Accéder au live",
+      url: "http://neosportek.blogspot.com/p/world-cup.html"
+    }
+  ];
+
+  sendMessageQueue.push({ match, event, msg, attachments });
+};
+
+const handlePenaltyEvent = (match, event, team) => {
+  console.log("New event: penalty");
+
+  const oppTeam = match.getOppositeTeam(team);
+
+  let msg = `:exclamation: *Penalty* accordé ${oppTeam.getNameWithDeterminer(
+    "à",
+    true
+  )} (${event.MatchMinute})`;
+
+  sendMessageQueue.push({ match, event, msg });
+};
+
+const handlePenaltyMissedEvent = (match, event, team, type) => {
+  console.log("New event: penaltyMissed");
+
+  const oppTeam = match.getOppositeTeam(team);
+
+  let msg = `:no_good: *Penalty raté* par ${oppTeam.getNameWithDeterminer(
+    null,
+    true
+  )} (${event.MatchMinute})`;
+
+  sendMessageQueue.push({ match, event, msg });
+};
+
+const handleComingUpMatchEvent = match => {
+  console.log("New event: comingUpMatch");
+
+  const diff = Math.ceil(Math.abs(getNow().diff(match.getDate()) / 1000 / 60));
+
+  const msg = `:soon: *Le match commence bientôt* (${diff} min)`;
+
+  const attachments = [
+    {
+      title: ">> Pensez à vos pronos <<",
+      title_link: "https://www.monpetitprono.com/forecast/matches-to-come"
+    }
+  ];
+
+  sendMessageQueue.push({ match, msg, attachments });
+};
+
 const createMatch = data => {
   const match = new Match(data);
 
